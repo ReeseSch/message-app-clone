95a0df762b260a6444bb2b1aa776c0faaf8a9d73
Reese Schumacher <Reese@Reeses-MacBook-Pro.local>
2021-10-05 13:48:16 -0600

    initial commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,35 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { Text, StyleSheet, View, Image } from 'react-native'
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+export default function TabOneScreen(){
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Image source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image} />
+
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+
+  text: {
+    fontSize: 30,
+    color: 'red'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+
+  image: {
+    width: 100,
+    height: 100
+  }
+
+
+
+})
+
